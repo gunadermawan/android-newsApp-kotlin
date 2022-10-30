6fd6fa4f3c5f1268acf1cd9b8dd58ff9719b385b
gunadermawan <gunadermawan.official@gmail.com>
2022-10-30 19:01:21 +0700

    make fun bookmark

--- app/src/main/java/com/gundermac/newsapss/core/di/ViewmodelModule.kt
+++ app/src/main/java/com/gundermac/newsapss/core/di/ViewmodelModule.kt
@@ -1,8 +1,13 @@
 package com.gundermac.newsapss.core.di
 
+import com.gundermac.newsapss.ui.detail.DetailViewModel
 import com.gundermac.newsapss.ui.home.HomeViewModel
 import org.koin.dsl.module
 
-val viewModelMode = module {
+val HomeViewModelModule = module {
     factory { HomeViewModel(get()) }
+}
+
+val DetailViewModelModule = module {
+    factory { DetailViewModel(get()) }
 }
--- app/src/main/java/com/gundermac/newsapss/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/gundermac/newsapss/ui/detail/DetailActivity.kt
@@ -5,16 +5,17 @@ import android.view.Menu
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gundermac.newsapss.R
 import com.gundermac.newsapss.core.data.source.remote.model.ArticleModel
 import com.gundermac.newsapss.databinding.ActivityDetailBinding
 import com.gundermac.newsapss.databinding.CustomToolbarBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
     private lateinit var bindinToolbar: CustomToolbarBinding
+    private val viewModel: DetailViewModel by viewModel()
     private val detail by lazy {
         intent.getSerializableExtra("intent_detail_news") as ArticleModel
     }
@@ -30,6 +31,7 @@ class DetailActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
         }
         detail.let {
+            viewModel.find(it)
             val web = binding.webView
             web.loadUrl(it.url!!)
             web.webViewClient = object : WebViewClient() {
@@ -52,9 +54,12 @@ class DetailActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_bookmark, menu)
         val menuBookmark = menu!!.findItem(R.id.action_bookmark)
         menuBookmark.setOnMenuItemClickListener {
-            Toast.makeText(applicationContext, "add to bookmark", Toast.LENGTH_SHORT).show()
-            menuBookmark.setIcon(R.drawable.ic_bookmark_true)
+            viewModel.bookmark(detail)
             true
+        }
+        viewModel.isBookmark.observe(this) {
+            if (it == 0) menuBookmark.setIcon(R.drawable.ic_bookmark_false)
+            else menuBookmark.setIcon(R.drawable.ic_bookmark_true)
         }
         return super.onCreateOptionsMenu(menu)
     }
