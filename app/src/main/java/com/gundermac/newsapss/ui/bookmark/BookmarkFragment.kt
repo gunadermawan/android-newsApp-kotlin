14770506c930be46b235eb4b7f640aa4ffd2bde6
gunadermawan <gunadermawan.official@gmail.com>
2023-01-24 22:31:20 +0700

    update build gradle

--- app/src/main/java/com/gundermac/newsapss/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/gundermac/newsapss/ui/bookmark/BookmarkFragment.kt
@@ -21,7 +21,7 @@ class BookmarkFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentBookmarkBinding.inflate(inflater, container, false)
         bindingToolbar = binding.toolbar
         return binding.root
